8492b808b8cc956097181cb95bb1ae5d8b1375ac
Sachin Mamoru <sachinmamoru@gmail.com>
2022-09-18 13:22:37 +0530

    fixed pipeline

--- main.tf
+++ main.tf
@@ -11,6 +11,11 @@ terraform {
   }
 }
 
+variable "imagebuild" {
+  type = string
+  description = "Latest Image Build"
+}
+
 resource "azurerm_resource_group" "tf_test" {
   name = "tfmainrg"
   location = "eastus"
@@ -27,7 +32,7 @@ resource "azurerm_container_group" "tfcg_test" {
 
   container {
       name            = "weatherapi"
-      image           = "sachinm4d/wapi"
+      image           = "sachinm4d/wapi:${imagebuild}"
         cpu             = "1"
         memory          = "1"
 
