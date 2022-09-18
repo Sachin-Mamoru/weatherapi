bd1bc46eb2af14c19e1fc4a3b7ddcf289c1331a1
Sachin Mamoru <sachinmamoru@gmail.com>
2022-09-19 00:59:59 +0530

    fixed pipeline vars

--- variables.tf
+++ variables.tf
@@ -1,10 +1,12 @@
 variable "secret_name" {
   type        = string
+  default = "name"
   description = "Key Vault Secret name in Azure"
 }
 
 variable "secret_value" {
   type        = string
+  default = "value"
   description = "Key Vault Secret value in Azure"
   sensitive   = true
 }
