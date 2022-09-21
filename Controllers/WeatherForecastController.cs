1d453a50e30dbc9b429450fc732847fb413bf005
Sachin Mamoru <sachinmamoru@gmail.com>
2022-09-21 10:08:39 +0530

    api changes

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmys", "Hot", "Swelterings", "Scorchings"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmys", "Hot", "Sweltering", "Scorchings"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
